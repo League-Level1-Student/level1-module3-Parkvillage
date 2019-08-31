713f64b1d54f8722068d905fb4eaf978349a46f2
sanjay marathe <37644745+Parkvillage@users.noreply.github.com>
2019-08-31 11:21:41 -0700

    Add files via upload

--- src/_07_cow_timer/CowTimer.java
+++ src/_07_cow_timer/CowTimer.java
@@ -5,13 +5,23 @@ package _07_cow_timer;
  */
 
 import java.applet.AudioClip;
+import java.awt.Component;
 import java.io.IOException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class CowTimer {
 
 	/* 1. Make a constructor for the CowTimer class that initializes the minutes variable */
-	
+	 CowTimer(){
+		minutes= 5;
+	}
 	/* 4. Complete the main method of the CowTimerRunner class */
 
 	private int minutes;
@@ -26,15 +36,32 @@ public class CowTimer {
 		 * 2. Count down the minutes, print the current minute then sleep for the number of minutes
 		 * using Thread.sleep(int milliseconds). 
 		 */
-
+		JOptionPane.showMessageDialog(null,"Do you want to cut the green or red wire");
+		playSound("countdown.wav");
+		for(int i=minutes; i>=0; i--) {
+			System.out.println(i);
+			Thread.sleep(1000);
+		}
+		playSound("explosion.wav");
+		JFrame frame= new JFrame();
+		frame.setVisible(true);
+		JLabel label= new JLabel();
+        label=loadImageFromWithinProject("explosionimg.jpg");
+        frame.add(label);
+        frame.pack();
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
 		 * from freesound.org, then drag it intothe default package.
 		 */
+		
 
 	}
-
+	public JLabel loadImageFromWithinProject(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imageURL);
+		return new JLabel(icon);
+	}
 	private void playSound(String fileName) {
 		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
 		sound.play();
